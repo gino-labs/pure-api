ccb76d46dbd6386627cb316316f974325ad60ade
Gino Curtis <gxc@rincon.com>
2025-05-08 11:47:16 -0700

    add: parser function to include postional and option --file

--- purestorage/pure-migration-v3.py
+++ purestorage/pure-migration-v3.py
@@ -23,24 +23,52 @@ API_TOKEN_S200 = ""                                #
 MIGRATION_POLICY = ""                              #
 ####################################################
 
+#######################
 ### Non-API Section ###
+#######################
+
+# Parse CLI arguments of filesystem names
+def Parse_FS_Args():
+    parser = argparse.ArgumentParser(
+        description="Python Script to migrate filesystems using pcopy and rsync",
+    )
+
+    parser.add_argument("filesystems", nargs="*", help="One or more filesystem names")
+    parser.add_argument("--file", help="Path to a filesystem")
+
+    args = parser.parse_args()
+
+    if args.file:
+        cwd = os.getcwd()
+        fs_file = f"{cwd}/{args.file}"
+        if os.path.isfile(fs_file):
+            with open(fs_file, "r") as f:
+                lines = f.read().splitlines()
+                return lines
+        else:
+            print(f"{fs_file} is not a file.")
+    else:
+        return args.filesystems
 
 
-
+#######################
 ### GET API Section ###
+#######################
 
 
-
+########################
 ### POST API Section ###
+########################
 
 
-
+#########################
 ### PATCH API Section ###
+#########################
 
 
-
+##########################
 ### DELETE API Section ###
-
+##########################
 
 
 ### Main Function ###
