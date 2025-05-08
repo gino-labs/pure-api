c9fb4711ce77df6ca2ce481f3b749ac5c41643f8
Gino Curtis <gxc@rincon.com>
2025-05-08 11:19:29 -0700

    add: migration v3 libraries and vars

--- purestorage/pure-migration-v3.py
+++ purestorage/pure-migration-v3.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+import os
+import sys
+import time
+import json
+import urllib3
+import requests
+import argparse
+import subprocess
+from datetime import datetime
+
+# Disabling Insecure Requests Warning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+################# GLOBAL VARIABLES #################
+PB1 = ""                                           #
+PB2 = ""                                           #
+PB1_MGT = ""                                       #
+PB2_MGT = ""                                       #
+LOCAL_IP = ""                                      #
+API_TOKEN = os.getenv("API_TOKEN")                 #
+API_TOKEN_S200 = os.getenv("API_TOKEN_S200")       #
+MIGRATION_POLICY = ""                              #
+####################################################
