2ee95ab35b10cd34d9dce393d6b651533d8e5697
Gino Curtis <gxc@rincon.com>
2025-05-31 13:37:04 -0700

    change: copy to sync, rclone subprocess

--- purestorage/object_storage_migration.py
+++ purestorage/object_storage_migration.py
@@ -190,7 +190,7 @@ def Migrate_Objects():
         # Rclone subprocess
         for buck in bucket_matches:
             try:
-                subprocess.run(["rclone", "copy", f"srcfb:{buck}", f"destfb:{buck}", "--config", "rclone.conf", "--progress", "-vv", "--no-check-certificate"])
+                subprocess.run(["rclone", "sync", f"srcfb:{buck}", f"destfb:{buck}", "--config", "rclone.conf", "--progress", "-vv", "--no-check-certificate"])
                 print(f"Successful rclone of {acct_name} and {buck}")
                 print()
                 time.sleep(3)
