cd699a6744904355f5fcedbdf2e05325d36083af
Gino Curtis <gxc@rincon.com>
2025-06-18 12:15:52 -0700

    add: Missing key

--- purestorage/test.py
+++ purestorage/test.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     
     del fs["promotion_status"]
     del fs["created"]
-    del fs["nfs"]["resource_type"]
+    del fs["nfs"]["export_policy"]["resource_type"]
 
     print(json.dumps(fs, indent=4))
 
