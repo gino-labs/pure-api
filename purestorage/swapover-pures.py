ad078b4bf0427d0b06eb7cee668b708b3f67ce20
Gino Curtis <curtis.gino@rincon.com>
2025-07-15 11:12:01 -0700

    add: testing for demote, disable replia links

--- purestorage/swapover-pures.py
+++ purestorage/swapover-pures.py
@@ -8,11 +8,29 @@ auth_token = pv3.Get_Session_Token(pv3.API_TOKEN, pv3.PB1_MGT)
 auth_token_s200 = pv3.Get_Session_Token(pv3.API_TOKEN_S200, pv3.PB2_MGT)
 
 # Get List of Filesystems to Demote on Legacy
-filesystems = pv3.Get_Filesystems(auth_token, pv3.PB1_MGT)
-filesystems = pv3.Get_Filesystems(auth_token_s200, pv3.PB2_MGT)
+#pv3.Get_Filesystems(auth_token, pv3.PB1_MGT)
+#pv3.Get_Filesystems(auth_token_s200, pv3.PB2_MGT)
+filesystems = [
+    {
+        "name": "gxc_test"
+    }
+]
+
+filesystems200 = [
+    {
+        "name": "gxc_test"
+    }
+]
+
+fs_names = []
+for fs in filesystems200:
+    fs_names.append(fs["name"])
 
 # For each filesystem disable / demote
-payload = {
+demote_payload = {
+    "http": {
+        "enabled": False
+    },
     "nfs": {
         "v3_enabled": False,
         "v4_1enabled": False
@@ -22,9 +40,11 @@ payload = {
 for fs in filesystems:
 ### pv3.Patch_Fs(fs["name"], auth_token, pv3.API_TOKEN, payload)
     print(f"Would patch {fs['name']} with the following json payload...\n")
-    print(json.dumps(payload, indent=4))
+    print(json.dumps(demote_payload, indent=4))
 
 # Get list of filesystems to Promote on S200
+
+# For each filesystem replica link disable
 
 # For each filesystem enable / promote
 
