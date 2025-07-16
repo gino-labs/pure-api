36349398f17118677558d6d670e53f20824cc3a1
Gino Curtis <curtis.gino@rincon.com>
2025-07-16 11:54:23 -0700

    debug: more testing with gxc_test filesystem

--- purestorage/test.py
+++ purestorage/test.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     pv3.Get_API_Versions(auth_token_s200, pv3.PB2_MGT)
 
 
-    print(json.dumps(pv3.Get_Single_Filesystem("anaconda_linux_chantilly", auth_token, pv3.PB1_MGT), indent=4))
+    print(json.dumps(pv3.Get_Single_Filesystem("gxc_test", auth_token, pv3.PB1_MGT), indent=4))
     
     '''
     payload = {
