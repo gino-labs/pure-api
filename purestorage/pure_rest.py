4c4dbb0083267841df0317a51e7fe4296386138f
Gino Curtis <curtis.gino@rincon.com>
2025-07-22 14:23:23 -0700

    add: Basic initialization of attributes with FlashBladeAPI class

--- purestorage/pure_rest.py
+++ purestorage/pure_rest.py
@@ -20,3 +20,35 @@ API_TOKEN_S200 = os.getenv("API_TOKEN_S200")                #
 MIGRATION_POLICY = os.getenv("MIGRATION_POLICY")            #
 REPLICATION_CUTOFF = os.getenv("REPLICATION_CUTOFF")        #
 #############################################################
+
+class FlashBladeAPI():
+    def __init__(self, data_ip, mgt_ip, api_token):
+        self.data_ip = data_ip
+        self.mgt_ip = mgt_ip
+        self.api_token = api_token
+        self.baseurl = f"https://{mgt_ip}/api/2.latest/"
+        self.auth_token = self.Get_Session_Token()
+        self.auth_headers = self.Set_Auth_Headers()
+
+    def Get_Session_Token(self):
+        url = f"https://{self.mgt_ip}/api/login"
+
+        headers = {
+        "api-token": self.api_token,
+        "Content-Type": "application/json"
+    }
+        response = requests.post(url, headers=headers, verify=False)
+
+        if response.status_code == 200:
+            return response.headers.get("x-auth-token")
+        else:
+            print(f"Login failed. Status Code: {response.status_code}\n{response.text}")
+            print()
+            return None
+        
+    def Set_Auth_Headers(self):
+        headers = {
+            "x-auth-token": self.auth_token,
+            "Content-Type": "application/json"
+        }
+        return headers
