abf57cb4972a0256aaba77530412863dccd4e6d6
Gino Curtis <curtis.gino@rincon.com>
2025-07-22 13:49:36 -0700

    edit: rename variables section

--- purestorage/pure_rest.py
+++ purestorage/pure_rest.py
@@ -9,14 +9,14 @@ from datetime import datetime
 # Disabling Insecure Requests Warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-################### GLOBAL VARIABLES ###################  
-PB1 = os.getenv("PB1")                                 #
-PB2 = os.getenv("PB2")                                 #
-PB1_MGT = os.getenv("PB1_MGT")                         # 
-PB2_MGT = os.getenv("PB2_MGT")                         #
-LOCAL_IP = os.getenv("LOCAL_IP")                       #
-API_TOKEN = os.getenv("API_TOKEN")                     #
-API_TOKEN_S200 = os.getenv("API_TOKEN_S200")           #
-MIGRATION_POLICY = os.getenv("MIGRATION_POLICY")       #
-REPLICATION_CUTOFF = os.getenv("REPLICATION_CUTOFF")   #
-########################################################
+################### ENVIRONMENT VARIABLES ###################  
+PB1 = os.getenv("PB1")                                      #
+PB2 = os.getenv("PB2")                                      #
+PB1_MGT = os.getenv("PB1_MGT")                              #
+PB2_MGT = os.getenv("PB2_MGT")                              #
+LOCAL_IP = os.getenv("LOCAL_IP")                            #
+API_TOKEN = os.getenv("API_TOKEN")                          #
+API_TOKEN_S200 = os.getenv("API_TOKEN_S200")                #
+MIGRATION_POLICY = os.getenv("MIGRATION_POLICY")            #
+REPLICATION_CUTOFF = os.getenv("REPLICATION_CUTOFF")        #
+#############################################################
