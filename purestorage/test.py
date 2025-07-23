ee6d08e7b4d41ea624eb96cf344f03a84a98ace1
Gino Curtis <curtis.gino@rincon.com>
2025-07-23 13:11:06 -0700

    debug: test snapshot get method

--- purestorage/test.py
+++ purestorage/test.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
     s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
 
-    test = legacy.get_single_filesystem("gxc_test")
+    test = legacy.get_single_filesystem_snapshot("gxc_test")
 
     print(test)
