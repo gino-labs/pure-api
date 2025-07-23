ff1aa4ca1d240f0891f5abad97f2b4e777f651a6
Gino Curtis <curtis.gino@rincon.com>
2025-07-23 10:30:42 -0700

    edit: test.py to try FlashBladeAPI class

--- purestorage/test.py
+++ purestorage/test.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 import json
 import time
+import purefb_api as pfa
 import pure_migration_v3 as pv3
 
 
 if __name__ == "__main__":
-    auth_token = pv3.Get_Session_Token(pv3.API_TOKEN, pv3.PB1_MGT, message=False)
-    auth_token_s200 = pv3.Get_Session_Token(pv3.API_TOKEN_S200, pv3.PB2_MGT, message=False)
+    legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
+    s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
 
-    #pv3.Get_API_Versions(auth_token, pv3.PB1_MGT)
-    #pv3.Get_API_Versions(auth_token_s200, pv3.PB2_MGT)
-
-    result = pv3.Get_Single_Filesystem_Snapshot("pre-swap", auth_token, pv3.PB1_MGT)
-
-    print(json.dumps(result, indent=4))
+    legacy.get_single_filesystem("gxc_test", dumpjson=True)
