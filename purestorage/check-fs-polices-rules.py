ef86df2bd8061510a56ebbc99051135f0e762329
Gino Curtis <curtis.gino@rincon.com>
2025-08-04 13:49:07 -0700

    feat: add initial logic for comparing nfs rules from two flashblades

--- purestorage/check-fs-polices-rules.py
+++ purestorage/check-fs-polices-rules.py
@@ -6,8 +6,29 @@ import json
 legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
 s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
 
+legacy_filesystems = legacy.get_filesystems()
 s200_filesystems = s200.get_filesystems()
 
-for fs in s200_filesystems:
-    print(json.dumps(fs["nfs"], indent=4))
+if len(legacy_filesystems) != len(s200_filesystems):
+    print(f"Mismatch filesystems count. s200:{len(s200_filesystems)}, legacy:{len(legacy_filesystems)}")
     print()
+    exit()
+
+for fs in s200_filesystems:
+    fs_name = fs["name"]
+    nfs_rules = fs["nfs"]["rules"]
+    legacy_nfs_rules = None
+    for fs_legacy in legacy_filesystems:
+        if fs_name == fs_legacy["name"]:
+            legacy_nfs_rules = fs_legacy["nfs"]["rules"]
+    if legacy_nfs_rules == None:
+        print(f"Could not find matching filesystem named {fs_name}")
+        print()
+    elif legacy_nfs_rules not in nfs_rules:
+        print(f"Legacy NFS rules not found in s200 filesystem: {fs_name}")
+        print(f"s200: {json.dumps(nfs_rules)}")
+        print(f"Legacy: {json.dumps(legacy_nfs_rules)}")
+        print()
+    else:
+        print(f"NFS rules okay: {fs_name}")
+        print()
