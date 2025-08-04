fb8c8e1eb782c2afff5f921dc96e371ddb772d4d
Gino Curtis <curtis.gino@rincon.com>
2025-08-04 14:12:51 -0700

    refactor: change nfs rules check into a function

--- purestorage/check-fs-polices-rules.py
+++ purestorage/check-fs-polices-rules.py
@@ -2,37 +2,38 @@
 import purefb_api as pfa
 import json
 
-# Initialize api sessions to each blade
-legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
-s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
+def compare_nfs_rules():
+    # Initialize api sessions to each blade
+    legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
+    s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
 
-legacy_filesystems = legacy.get_filesystems()
-s200_filesystems = s200.get_filesystems()
+    legacy_filesystems = legacy.get_filesystems()
+    s200_filesystems = s200.get_filesystems()
 
-if len(legacy_filesystems) != len(s200_filesystems):
-    print(f"Mismatch filesystems count. s200:{len(s200_filesystems)}, legacy:{len(legacy_filesystems)}")
-    print()
-    exit()
+    if len(legacy_filesystems) != len(s200_filesystems):
+        print(f"Mismatch filesystems count. s200:{len(s200_filesystems)}, legacy:{len(legacy_filesystems)}")
+        print()
+        exit()
 
-for fs in s200_filesystems:
-    fs_name = fs["name"]
-    nfs_rules = fs["nfs"]["rules"]
-    legacy_nfs_rules = None
-    for fs_legacy in legacy_filesystems:
-        if fs_name == fs_legacy["name"]:
-            legacy_nfs_rules = fs_legacy["nfs"]["rules"]
-    if legacy_nfs_rules == None:
-        print(f"Could not find matching filesystem named {fs_name}")
-        print()
-    elif legacy_nfs_rules not in nfs_rules:
-        print(f"Legacy filesystem {fs_name} NFS rules not found in s200 filesystem:")
-        print()
-        rules_list = nfs_rules.split(" ")
-        legacy_rules_list = legacy_nfs_rules.split(" ")
-        for rule in legacy_rules_list:
-            if rule not in rules_list:
-                print(rule)
-        print()
-    else:
-        print(f"NFS rules OK: {fs_name}")
-        print()
+    for fs in s200_filesystems:
+        fs_name = fs["name"]
+        nfs_rules = fs["nfs"]["rules"]
+        legacy_nfs_rules = None
+        for fs_legacy in legacy_filesystems:
+            if fs_name == fs_legacy["name"]:
+                legacy_nfs_rules = fs_legacy["nfs"]["rules"]
+        if legacy_nfs_rules == None:
+            print(f"Could not find matching filesystem named {fs_name}")
+            print()
+        elif legacy_nfs_rules not in nfs_rules:
+            print(f"Legacy filesystem {fs_name} NFS rules not found in s200 filesystem:")
+            print()
+            rules_list = nfs_rules.split(" ")
+            legacy_rules_list = legacy_nfs_rules.split(" ")
+            for rule in legacy_rules_list:
+                if rule not in rules_list:
+                    print(rule)
+            print()
+        else:
+            print(f"NFS rules OK: {fs_name}")
+            print()
