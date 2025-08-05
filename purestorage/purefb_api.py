35029528100889b16b89454b24925911ba3660a2
Gino Curtis <curtis.gino@rincon.com>
2025-08-05 08:29:21 -0700

    fix: typo in url string

--- purestorage/purefb_api.py
+++ purestorage/purefb_api.py
@@ -302,7 +302,7 @@ class FlashBladeAPI():
         
     # Get filesystems snapshot policies
     def get_filesystems_snapshot_policies(self, filesystem, dumpjson=False):
-        url = self.baseurl + f"file-systems/polices?member_names={filesystem}"
+        url = self.baseurl + f"file-systems/policies?member_names={filesystem}"
         msg = f"filesystem {filesystem} snapshot policies"
         data = self.REST_Request("get", url, msg)
 
