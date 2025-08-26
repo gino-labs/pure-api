b4f780865d25567babd9802c64ce0d43cbb21db7
Gino Curtis <curtis.gino@rincon.com>
2025-08-26 10:53:57 -0700

    feat: add migration function for object store accounts

--- purestorage/fbmigrate_object_data.py
+++ purestorage/fbmigrate_object_data.py
@@ -6,6 +6,23 @@ import subprocess
 import os
 
 # Migrate object store accounts
+def migrate_object_store_accounts():
+    legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
+    s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
+
+    accts = legacy.get_object_store_accounts()
+    s200_accts = s200.get_object_store_accounts()
+
+    s200_acct_names = [acct["name"] for acct in s200_accts]
+
+    for acct in accts:
+        payload = {
+            "bucket_defaults": acct["bucket_defaults"],
+            "hard_limit_enabled": acct["hard_limit_enabled"],
+            "quota_limit": acct["quota_limit"]
+        }
+        if acct["name"] not in s200_acct_names:
+            s200.post_object_store_account(acct["name"], payload)
 
 # Migrate buckets
 
