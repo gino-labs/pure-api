befd38bb4dc3903d1e2365b59fefc6cfa5a6c86b
Gino Curtis <curtis.gino@rincon.com>
2025-08-26 10:21:47 -0700

    chore: comment general steps for object migration

--- purestorage/fbmigrate_object_data.py
+++ purestorage/fbmigrate_object_data.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 import purefb_api as pfa
 import purefb_log as pfl
+
+# Migrate object store accounts
+
+# Migrate buckets
+
+# Migrate object store users
+
+# Create new object store access/secret keys for users on both FBs (Save secrets for s200)
+
+# Migrate object storage using rclone
+
+# Establish object replication link after using s200 created keys
+
+# Remove temporary object store users on legacy used for rclone
