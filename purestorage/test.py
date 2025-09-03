4a445e6a5b65d9cc945d7d412b85ddf9ec8d3271
Gino Curtis <curtis.gino@rincon.com>
2025-09-03 14:38:47 -0700

    fix: variable name argument

--- purestorage/test.py
+++ purestorage/test.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
         "paused": False,
         "cascading_enabled": False
     }
-    legacy.post_bucket_replica_link("gxc-bucket", credential, payload)
+    legacy.post_bucket_replica_link("gxc-bucket", credential_name, payload)
 
