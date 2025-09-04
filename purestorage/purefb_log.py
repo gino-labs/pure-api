48955abfd7b06e0c10d770c9ce1959a3cc7a59bc
Gino Curtis <curtis.gino@rincon.com>
2025-09-04 10:33:11 -0700

    feat: add show output parameter to print msg/data to terminal

--- purestorage/purefb_log.py
+++ purestorage/purefb_log.py
@@ -12,7 +12,7 @@ class PureLog:
         formatted_timestamp = f"{now.strftime('%d%b%Y-%H:%M:%S')}"
         return formatted_timestamp
 
-    def write_log(self, message, jsondata=None):
+    def write_log(self, message, jsondata=None, show_output=False):
         os.makedirs(self.logdir, exist_ok=True)
         todays_log = os.path.join(self.logdir, self.logfile)
         stamp = self.timestamp()
@@ -20,8 +20,15 @@ class PureLog:
         with open(todays_log, 'a') as log:
             log.write(f"[{stamp}] {message}\n")
 
+        if show_output:
+            print(message)
+            print()
+
         if jsondata:
             with open(todays_log, 'a') as log:
                 json.dump(jsondata, log, indent=4)
                 log.write("\n")
+            if show_output:
+                print(json.dumps(jsondata, indent=4))
+                print()
         
