f32ab42cad05d0b1871a9f42e24324f27d0f4492
Gino Curtis <curtis.gino@rincon.com>
2025-09-15 19:26:02 -0700

    refactor: adding more polished logic to flashblade swap script

--- purestorage/swap_flashblades.py
+++ purestorage/swap_flashblades.py
@@ -15,27 +15,58 @@ legacy = pfa.FlashBladeAPI(pfa.PB1, pfa.PB1_MGT, pfa.API_TOKEN)
 s200 = pfa.FlashBladeAPI(pfa.PB2, pfa.PB2_MGT, pfa.API_TOKEN_S200)
 
 # Get Legacy file systems
-legacy.get_filesystems()
+legacy_filesystems = legacy.get_filesystems()
 
 # Get S200 file systems
-s200.get_filesystems()
+s200_filesystems = s200.get_filesystems()
+
+s200_filesystem_names = [fs["name"] for fs in s200_filesystems]
 
 # Get Legacy interfaces' info
-legacy.get_interfaces()
+legacy_interfaces = legacy.get_interfaces()
+
+legacy_data_iface_names = []
+legacy_data_ips = []
+
+for iface in legacy_interfaces:
+    if "data" in iface["services"]:
+        legacy_data_iface_names.append(iface["name"])
+        legacy_data_ips.append(iface["address"])
 
 # Store Production IPs in variable to pass to ansible playbook later
+if len(legacy_data_ips) > 1:
+    production_ips = "|".join(legacy_data_ips)
+elif len(legacy_data_ips) == 1:
+    production_ips = legacy_data_ips[0]
+else:
+    print("No data interfaces.")
+    print()
 
 # Get S200 interfaces' info
-s200.get_interfaces()
+s200_interfaces = s200.get_interfaces()
+
+s200_data_iface_names = [iface["name"] for iface in s200_interfaces if "data" in iface["name"]]
 
 # Get file system replica links on Legacy
-legacy.get_filesytem_replica_links()
+legacy_replica_links = legacy.get_filesytem_replica_links()
 
 # Get active NFS clients before swapping
 scriptlog.write_log("Retrieving active NFS clients from Legacy FlashBlade. Reload Cache in progress...", show_output=True)
-legacy.get_nfs_clients()
+hosts = legacy.get_nfs_clients()
 
 # Create inventory file with NFS clients obtained
+
+inventory = {
+    "all": {
+        "hosts": {host.split(":")[0]: None for host in hosts if "172.20.0" not in host}
+    }
+}
+
+inventory_filename = s200.mgt_ip[:2] + "_inventory.json"
+
+os.makedirs("logs", exist_ok=True)
+with open(f"log/{inventory_filename}", "w") as inv_file:
+    json.dump(inventory, inv_file, indent=4)
 
 # Create final snapshots on Legacy and wait 30 seconds for them to settle
 
