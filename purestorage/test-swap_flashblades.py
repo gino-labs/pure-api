4457c3177b7dab4125945072eb74b942cf31f043
Gino Curtis <curtis.gino@rincon.com>
2025-09-18 10:07:02 -0700

    refactor: add more debug a simulated output

--- purestorage/test-swap_flashblades.py
+++ purestorage/test-swap_flashblades.py
@@ -2,8 +2,6 @@
 import purefb_api as pfa
 import purefb_log as pfl
 import subprocess
-import tempfile
-import time
 import json
 import os
 
@@ -75,25 +73,60 @@ with open(f"logs/{inventory_filename}", "w") as inv_file:
     json.dump(inventory, inv_file, indent=4)
 
 # Create final snapshots on Legacy and wait 30 seconds for them to settle
-scriptlog.write_log("Create snapshots for every file system and wait 30 seconds.", show_output=True)
+fs_snapshot_list = [fs["name"] for fs in legacy_filesystems if fs["promotion_status"] == "promoted"]
+scriptlog.write_log("Create snapshots for promoted file systems and wait 30 seconds. (See JSON data)", jsondata=fs_snapshot_list, show_output=True)
 
 # Demote / Disable each file system on Legacy (Handle exception: non-replication snapshot error, skip demotion)
-scriptlog.write_log("Demote Legacy filesystems, handle non replication snapshots.", show_output=True)
+demote_payload = {
+    "writable": False,
+    "requested_promotion_state": "demoted"
+}
+scriptlog.write_log("Using demote payload", jsondata=demote_payload, show_output=True)
+
+fs_demote_list = [fs["name"] for fs in legacy_filesystems]
+scriptlog.write_log("Demote Legacy filesystems, handle non replication snapshots.", jsondata=fs_demote_list, show_output=True)
 
 # Patch Legacy IPs to S200
 scriptlog.write_log("Patch Legacy IPs to S200.", show_output=True)
+s200_new_ips = [iface["address"] for iface in legacy_interfaces if "data" in iface["services"]]
 
 # Patch S200 IPs to Legacy
 scriptlog.write_log("Patch S200 IPs to Legacy.", show_output=True)
+legacy_new_ips = [iface["address"] for iface in s200_interfaces if "data" in iface["services"]]
+
+ips_swapped = {
+    "s200": s200_new_ips,
+    "legacy": legacy_new_ips
+}
+scriptlog.write_log("New IPs that would be after swap.", jsondata=ips_swapped, show_output=True)
 
 # Delete replica links on Legacy
-scriptlog.write_log("Delete replica links on Legacy.", show_output=True)
+fs_del_links_list = [link["local_file_system"]["name"] for link in legacy_replica_links]
+scriptlog.write_log("Delete replica links on Legacy.", jsondata=fs_del_links_list, show_output=True)
 
 # Promote / Enabled each file system on S200
+promote_payload = {
+    "nfs": {
+        "v3_enabled": "enabled",
+        "v4_1_enabled": "enabled",
+        "rules": "(rules-here)"
+    },
+    "http": {
+        "enabled": "disabled"
+    },
+    "writable": True,
+    "requested_promotion_state": "promoted"
+}
+
+scriptlog.write_log("Using promote payload.", jsondata=promote_payload, show_output=True)
+
+fs_promote_list = [fs["name"] for fs in s200_filesystems]
 scriptlog.write_log("Promote S200 file systems.", show_output=True)
 
 # Run ansible playbook with nfs client inventory and production IP variable
-scriptlog.write_log(f"Run ansible playbook to handle remounting nfs clients, pass egrep string: {production_ips}", show_output=True)
+scriptlog.write_log(f"Simulate (ping only) runnning ansible playbook to handle remounting nfs clients in create inventory, pass egrep string: {production_ips}", jsondata=inventory, show_output=True)
+print("Enter root password for ansible playbook.")
+subprocess.run(["ansible", "all", "-i", f"logs/{inventory_filename}"], "-m", "ping")
 
 # Clean up
 scriptlog.write_log("Clean up, remove files if needed.", show_output=True)
