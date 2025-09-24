83327de070f072325eedfecb67d9dea6c97f9c75
Gino Curtis <curtis.gino@rincon.com>
2025-09-24 10:28:11 -0700

    feat: add check which file systems don't have replication links

--- purestorage/fbmigration_checks.py
+++ purestorage/fbmigration_checks.py
@@ -11,3 +11,22 @@ What to check?
 {"errors":[{"code":32,"context":"tools_linux_chantilly","message":"The latest snapshot tools_linux_chantilly.2025_09_12_15_09 is not a replication snapshot.
  To demote a file system it must either have no snapshots or the most recent snapshot must be a replication snapshot."}]}
 '''
+
+logger = purefb_log.PureLog()
+
+legacy = purefb_api.FlashBladeAPI(purefb_api.PB1, purefb_api.PB1_MGT, purefb_api.API_TOKEN)
+s200 = purefb_api.FlashBladeAPI(purefb_api.PB2, purefb_api.PB2_MGT, purefb_api.API_TOKEN_S200)
+
+def check_replica_links_filesystems():
+    legacy_filesystems = legacy.get_filesystems()
+    legacy_replica_links = legacy.get_filesytem_replica_links()
+
+    legacy_fs_list = [fs["name"] for fs in legacy_filesystems]
+    legacy_replica_fs_list = [link["local_file_system"]["name"] for link in legacy_replica_links]
+
+    non_replica_fs_list = list(set(legacy_fs_list) - set(legacy_replica_fs_list))
+
+    print(non_replica_fs_list)
+
+if __name__ == "__main__":
+    check_replica_links_filesystems()
