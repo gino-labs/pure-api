1dd8450b467badfcde064bde0677e514aa554816
Gino Curtis <curtis.gino@rincon.com>
2025-09-25 08:09:55 -0700

    fix: if condition on nfs inventory build

--- purestorage/test-swap_flashblades.py
+++ purestorage/test-swap_flashblades.py
@@ -81,7 +81,7 @@ hosts = legacy.get_nfs_clients()
 
 inventory = {
     "all": {
-        "hosts": {host["name"].split(":")[0]: None for host in hosts["items"] if "172.20." not in host}
+        "hosts": {host["name"].split(":")[0]: None for host in hosts["items"] if "172.20." not in host["name"]}
     }
 }
 
