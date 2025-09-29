67f3f24da56118283bfe48153b18c35181334127
Gino Curtis <curtis.gino@rincon.com>
2025-09-29 09:04:19 -0700

    test: reproducing pre-swap snapshot api error

--- scripts/test.py
+++ scripts/test.py
@@ -17,34 +17,8 @@ if __name__ == "__main__":
     legacy_filesystems = legacy.get_filesystems()
     s200_filesystems = s200.get_filesystems()
 
-    # Create promotion payloads using corresponding legacy file system
-    s200_promo_payloads = {}
-    for fs in legacy_filesystems:
-        s200_promo_payloads[fs["name"]] = {
-            "nfs": {
-                "v3_enabled": fs["nfs"]["v3_enabled"],
-                "v4_1_enabled": fs["nfs"]["v4_1_enabled"],
-                "rules": fs["nfs"]["rules"]
-            },
-            "http": {
-                "enabled": fs["http"]["enabled"]
-            },
-            "writable": True,
-            "requested_promotion_state": "promoted" 
-        }
+    # Testing snapshots, trying to reproduce error for pre-swap snapshots during migration
 
-    logger.write_log("S200 file system promotion data from legacy", jsondata=s200_promo_payloads)
-
-    count = 0
-    destroyed_found = []
-    for fs in s200_filesystems:
-        if fs["name"] in s200_promo_payloads and not fs["destroyed"]:
-            count += 1       
-            logger.write_log(f"Filesystem {fs['name']} would be promoted: {count}", jsondata=s200_promo_payloads[fs["name"]], show_output=True)
-        if fs["destroyed"]:
-            destroyed_found.append(fs["name"])
-
-    logger.write_log("The following filesystems will not be promoted because they are destroyed:", jsondata=destroyed_found, show_output=True)
-    print(f"Non destroyed filesystem count: {count}")
+    anaconda_fs = legacy.get_filesystems(filesystems="anaconda_linux_tucson", dumpjson=True)
 
     watch.end_stopwatch()
