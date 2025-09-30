86c33da31e4d75cd860f4950b8495f5038461b16
Gino Curtis <curtis.gino@rincon.com>
2025-09-30 12:41:02 -0700

    feat: add todos and configmigrator class, with subnets migration method

--- scripts/fbmigrate_configs.py
+++ scripts/fbmigrate_configs.py
@@ -1,3 +1,72 @@
 #!/usr/bin/env python3
 from purefb_api import *
 from purefb_log import *
+
+
+'''
+TODO
+- Migrate/Configure Subnets/Vlans # Test
+- Migrate/Configure DNS
+- Migrate/Configure NTP
+- Migrate/Configure Policies
+- Migrate/Configure Directory Service
+- Migrate/Configure Certificates/Certificate-Group
+- Migrate/Configure Syslog Server Connections
+- Migrate/Configure Interfaces (Data/Replcation, ensure no duplicate IPs between blades)
+'''
+
+class ConfigMigrator:
+    def __init__(self, legacy, s200):
+        self.legacy = legacy
+        self.s200 = s200
+    
+    # Migrate subnets, verify name, vlan, subnet prefix
+    def migrate_config_subnets(self):
+        legacy_subnets = self.legacy.get_subnets()
+        s200_subnets = self.s200.get_subnets()
+
+        # Store existing S200 subnet details to not recreate overlapping
+        s200_sub_details = {
+            "s200_subnames": [],
+            "s200_prefixes": [],
+            "s200_vlans": []
+        }   
+        if s200_subnets:
+            if isinstance(s200_subnets, dict):
+                if "data" in s200_sub_details.get(s200_subnets["services"]):
+                    s200_sub_details["s200_subnames"].append(s200_subnets["name"])
+                    s200_sub_details["s200_prefixes"].append(s200_subnets["prefix"])
+                    s200_sub_details["s200_vlans"].append(s200_subnets["vlan"])
+            else:
+                for sub in s200_subnets:
+                    if "data" in s200_subnets.get(sub["services"], []):
+                        s200_sub_details["s200_subnames"].append(sub["name"])
+                        s200_sub_details["s200_prefixes"].append(sub["prefix"])
+                        s200_sub_details["s200_vlans"].append(sub["vlan"])
+
+        # For each subnet on legacy post subnet to s200
+        if legacy_subnets:
+            if isinstance(legacy_subnets, dict):
+                legacy_subnets = [legacy_subnets]
+            s200_subnames = s200_sub_details["s200_subnames"]
+            s200_prefixes = s200_sub_details["s200_prefixes"]
+            s200_vlans = s200_sub_details["s200_vlans"]
+            for sub in legacy_subnets:
+                if sub["name"] in s200_subnames:
+                    continue
+                if sub["prefix"] in s200_prefixes:
+                    continue
+                if sub["vlan"] in s200_vlans:
+                    continue
+
+                payload = {
+                    "gateway": sub["gateway"],
+                    "link_aggregation_group": sub["link_aggregation_group"]["name"],
+                    "mtu": sub["mtu"],
+                    "prefix": sub["prefix"],
+                    "vlan": sub["vlan"]
+                }
+                # Post subnets to s200
+                self.s200.post_subnet(sub["name"], payload)
+
+
