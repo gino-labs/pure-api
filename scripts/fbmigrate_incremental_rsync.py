f944e43462cc7bfe04acd5aca5e9387e32bfd30b
Gino Curtis <curtis.gino@rincon.com>
2025-10-03 15:30:25 -0700

    chore: start incremental rsync script

---
+++ scripts/fbmigrate_incremental_rsync.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from purefb_api import *
+from purefb_log import *
+from purefb_subprocess import PureSubprocessor
+
+# Define file systems that need to be migrated (Non-replication)
+
+# Make directories for mounting source and destination NFS mounts
+
+# Mount source and destination NFS file systems
+
+# Begin rsync of all non-replication file systems 
+
+# Unmount after filesystem is done rsyncing
+
+
