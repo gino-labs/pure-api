b0f5afe35732a34667392228b9371599ee5d5483
Gino Curtis <curtis.gino@rincon.com>
2025-10-06 10:08:21 -0700

    debug: dum json test_pol

--- scripts/test.py
+++ scripts/test.py
@@ -30,11 +30,12 @@ if __name__ == "__main__":
         ]
     }
     test_pol["rules"].append(new_rule)
+    print(json.dumps(test_pol, indent=4))
 
     payload = { 
         "rules": test_pol["rules"]
     }
-    print(json.dumps(payload, indent=4))
+
 
     s200.patch_nfs_export_policy("test", payload, dumpjson=True)
 
