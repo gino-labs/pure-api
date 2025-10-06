362d9c67257f52d442982fa0e3bc2183d0e21ef0
Gino Curtis <curtis.gino@rincon.com>
2025-10-06 07:06:37 -0700

    chore: remove unused comments

--- scripts/fbmigrate_incremental_rsync.py
+++ scripts/fbmigrate_incremental_rsync.py
@@ -41,12 +41,5 @@ def rsync_filesystem(filesystem):
 
 # Define file systems that need to be migrated (Non-replication)
 
-# Make directories for mounting source and destination NFS mounts
-
-# Mount source and destination NFS file systems
-
-# Begin rsync of all non-replication file systems 
-
-# Unmount after filesystem is done rsyncing
 
 
