1aff09e829eda440398f58b201c702187ce71a3b
Gino Curtis <curtis.gino@rincon.com>
2025-10-06 08:23:14 -0700

    refactor: dynamically determine local ip used to reach pure servers

--- scripts/site_vars/pure_env_template.sh
+++ scripts/site_vars/pure_env_template.sh
@@ -3,7 +3,9 @@ export PB1=''
 export PB2=''
 export PB1_MGT=''
 export PB2_MGT=''
-export LOCAL_IP=''
 export API_TOKEN=''
 export API_TOKEN_S200=''
 export REPLICATION_CUTOFF=''
+
+LOCAL_IP=$(ip route get $(getent hosts $PB2 | awk '{ print $1 }') | awk '{ print $5; exit}')
+export LOCAL_IP
