89cfcfaeac06694d1e7fd780f320763c1c0a6143
Gino Curtis <curtis.gino@rincon.com>
2025-10-07 12:42:36 -0700

    todo: working on idempotency with object migration

--- scripts/fbmigrate_object_data.py
+++ scripts/fbmigrate_object_data.py
@@ -21,17 +21,39 @@ legacy = FlashBladeAPI(*pb1_vars)
 s200 = FlashBladeAPI(*pb2_vars)
 
 # Helper function to skip migration of buckets with replica links already in place
-def check_bucket_replica_link(check_argument, check_user=False):
+def check_bucket_replica_links(check_argument, check_user=True, check_account=False):
     bucket_links = legacy.get_bucket_replia_links()
-    bucket_link_list = [bucket["local_bucket"]["name"] for bucket in bucket_links]
+    bucket_list_with_links = [bucket["local_bucket"]["name"] for bucket in bucket_links]
 
     buckets = legacy.get_buckets()
     
+    map_buckets_with_links = {}
     
-    map_buckets = {
+    for bucket in buckets:
+        if bucket["name"] in bucket_list_with_links:
+            account_name = bucket["account"]["name"]
+            users = legacy.get_object_store_users()
+            user_bucket_list = [user["name"] for user in users if user["account"]["name"] == account_name]
 
-    }
-    users = legacy.get_object_store_users()
+            map_buckets_with_links[bucket] = {
+                "account": bucket["account"]["name"],
+                "users": user_bucket_list 
+            }
+
+    if check_user:
+        for b in map_buckets_with_links:
+            for u in map_buckets_with_links[b]["users"]:
+                if u == check_argument:
+                    logger.write_log(f"Object store user {u} is associated with a bucket being replicated.")
+                    return True
+        return False
+    if check_account:
+        for b in map_buckets_with_links:
+            if map_buckets_with_links[b]["account"] == check_argument:
+                logger.write_log(f"Object store account {map_buckets_with_links[b]["account"]} is associated with a bucket being replicated.")
+                return True
+        return False
+    return False
 
 # Migrate object store accounts #
 def migrate_object_store_accounts():
@@ -50,6 +72,7 @@ def migrate_object_store_accounts():
         s200_acct_names = [acct["name"] for acct in s200_accts]
 
     # Post each account not in s200 account names
+    accts_migrated = []
     for acct in accts:
         if acct["name"] not in s200_acct_names:
             if acct["bucket_defaults"]["quota_limit"]:
@@ -71,6 +94,8 @@ def migrate_object_store_accounts():
                 "quota_limit": quota_limit
             }
             s200.post_object_store_account(acct["name"], payload)
+            accts_migrated.append[acct["name"]]
+    return accts_migrated
 
 # Migrate buckets #
 def migrate_buckets():
@@ -89,6 +114,7 @@ def migrate_buckets():
         s200_bucket_names = [bucket["name"] for bucket in s200_buckets]
 
     # Post each bucket not in s200 bucket names
+    buckets_migrated = []
     for bucket in buckets:
         if bucket["name"] not in s200_bucket_names:
             if bucket["object_lock_config"]["default_retention"]:
@@ -118,6 +144,8 @@ def migrate_buckets():
                 "retention_lock": bucket["retention_lock"]
             }
             s200.post_bucket(bucket["name"], payload)
+            buckets_migrated.append(bucket["name"])
+    return buckets_migrated
         
 # Migrate object store users #
 def migrate_object_store_users():
@@ -136,12 +164,15 @@ def migrate_object_store_users():
         s200_user_names = [user["name"] for user in s200_users]
 
     # Post each user not in s200 user names
+    users_migrated = []
     for user in users:
         if user["name"] not in s200_user_names and "migration" not in user["name"]:
             s200.post_object_store_user(user["name"])
+            users_migrated.append(user["name"])
+    return users_migrated
 
 # Create new object store access/secret keys for users on both FBs (Save secrets for s200)
-def create_new_s200_access_keys():
+def create_new_s200_access_keys(existing_users_list):
     print("New s200 access keys")
     print()
 
@@ -168,6 +199,8 @@ def create_new_s200_access_keys():
     key_data= []
     # Post new access keys from migrated users
     for user in s200_users:
+        if user in existing_users_list:
+            continue
         if os.path.exists(f".secrets/{s200.name}_s200_access_keys.json"):
             with open(f".secrets/{s200.name}_s200_access_keys.json", "r") as f:
                 s200_keys = json.load(f)
