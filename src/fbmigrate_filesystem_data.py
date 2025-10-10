a0c9db6c53782fa7ce057444ecd85389cff7b87a
CurtisGino <curtis.gino@rincon.com>
2025-10-10 16:25:26 -0700

    chore: adding more notes for future reference

--- src/fbmigrate_filesystem_data.py
+++ src/fbmigrate_filesystem_data.py
@@ -3,6 +3,20 @@ import purefb_api as pfa
 import purefb_log as pfl
 
 '''
+Example payload for legacy.post_filesystem_replica_link(filesystem, remote_array, payload)
+
+payload = {
+        "policies": [
+            {
+                "name": "5_mins",
+                "location": {
+                    "name": "bazpureblade1"
+                }
+            }
+        ]
+    }
+
+
 Api Error info for handling error in this script
 purefb_api.ApiError: [Code: 22] Replication is not supported for a file system that was created in a version prior to 3.0.0.
 '''
