454301e0e54d8b4a22284695da5aad6461062b38
CurtisGino <curtis.gino@rincon.com>
2025-10-10 16:21:33 -0700

    chore: comment error for future use

--- src/fbmigrate_filesystem_data.py
+++ src/fbmigrate_filesystem_data.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 import purefb_api as pfa
 import purefb_log as pfl
+
+'''
+Api Error info for handling error in this script
+purefb_api.ApiError: [Code: 22] Replication is not supported for a file system that was created in a version prior to 3.0.0.
+'''
