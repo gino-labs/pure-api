a255b39c778d0183a8d2b9b836986c240e8e0696
CurtisGino <curtis.gino@rincon.com>
2025-10-10 11:59:54 -0700

    feat: new script for reverting FlashBlade swap in case of disaster recovery.

---
+++ src/revert_swapfbs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from purefb_api import *
+from purefb_log import *
+
+# Using logs/configs/legacy_filesystems.json configure Legacy file systems to original state
+
+# Using logs/configs/s200_filesystems.json configure S200 file systems to original state
+
+# Using logs/configs/legacy_interfaces.json configure Legacy network interfaces to original state
+
+# Using logs/configs/s200_interfaces.json configure S200 network interfaces to original state
+
+# Re-establish file repliation links where possible
+
+# Re-establish object replication links where possible
