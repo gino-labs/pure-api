f0a6e04d3148d54e138f701decfb705bfc2ea713
CurtisGino <curtis.gino@rincon.com>
2025-10-23 12:58:59 -0700

    refactor: file name

--- src/tests/test_any.py
+++ src/tests/test_any.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         }
         user_filesystem_usages.append(curr_item)
 
-    with open("logs/azhome-user-usages.json", "w") as json_f:
+    with open(f"logs/home_linux_tucons_usages.json", "w") as json_f:
          json.dump(user_filesystem_usages, json_f, indent=4)
 
     watch.end_stopwatch()
