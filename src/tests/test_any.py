1510bfad0dc52481fa41bd8593557a79edbd31ce
CurtisGino <curtis.gino@rincon.com>
2025-10-23 12:20:03 -0700

    test: see raw json output

--- src/tests/test_any.py
+++ src/tests/test_any.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
 
     watch.start_stopwatch()
 
-    # quotas = s200.get_endpoint("quotas/users", f"file_system_names=home_linux_tucson&user_names=gxc", dumpjson=True)
+    quotas = s200.get_endpoint("quotas/users", f"file_system_names=home_linux_tucson", raw=True, dumpjson=True)
 
-    user_quotas = s200.get_user_quotas("home_linux_tucson", dumpjson=True)
+    #user_quotas = s200.get_user_quotas("home_linux_tucson", dumpjson=True)
 
-    with open("logs/azhome-user-quotas.json", "w") as json_f:
-        json.dump(user_quotas, json_f, indent=4)
+    # with open("logs/azhome-user-quotas.json", "w") as json_f:
+    #     json.dump(user_quotas, json_f, indent=4)
 
     watch.end_stopwatch()
