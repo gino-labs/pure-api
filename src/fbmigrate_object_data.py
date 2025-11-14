fbaff15abd708abecb20106e8483d556fc7c0342
CurtisGino <curtis.gino@rincon.com>
2025-11-14 09:22:11 -0700

    refactor: rlcone check before trying to migrate object storage

--- src/fbmigrate_object_data.py
+++ src/fbmigrate_object_data.py
@@ -469,15 +469,21 @@ if __name__ == "__main__":
     # Object Migrator instance
     obj_migrator = ObjectMigrator()
 
-    # Object migration operations
-    obj_migrator.delete_legacy_object_replica_links()
-    obj_migrator.delete_s200_access_keys()
-    obj_migrator.migrate_object_store_accounts()
-    obj_migrator.migrate_buckets()
-    obj_migrator.migrate_object_store_users()
-    obj_migrator.create_new_s200_access_keys()
-    obj_migrator.create_migration_legacy_users_and_keys()
-    obj_migrator.rclone_object_storage_buckets()
-    obj_migrator.remove_temporary_migration_users()
-    obj_migrator.add_remote_credentials()
-    obj_migrator.create_bucket_replica_links()
+    # Check rclone package is installed first
+    check_rclone = subprocess.run(["rpm", "-q", "rclone"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    if check_rclone.returncode != 0:
+        print("Rclone rpm package is not installed. Please install with \'sudo dnf install -y rclone\'", end="\n\n")
+    else:
+        # Object migration operations
+        obj_migrator.delete_legacy_object_replica_links()
+        obj_migrator.delete_s200_access_keys()
+        obj_migrator.migrate_object_store_accounts()
+        obj_migrator.migrate_buckets()
+        obj_migrator.migrate_object_store_users()
+        obj_migrator.create_new_s200_access_keys()
+        obj_migrator.create_migration_legacy_users_and_keys()
+        obj_migrator.rclone_object_storage_buckets()
+        obj_migrator.remove_temporary_migration_users()
+        obj_migrator.add_remote_credentials()
+        obj_migrator.create_bucket_replica_links()
