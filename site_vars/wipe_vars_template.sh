ebe36c18e3ac421d5a14a6fb07efc1ea5d80b492
CurtisGino <curtis.gino@rincon.com>
2025-12-08 15:20:01 -0700

    add: wipe environment template

---
+++ site_vars/wipe_vars_template.sh
@@ -0,0 +1,17 @@
+# AZ
+export AZFB_NAME=""
+export AZFB_MGT=""
+export AZFB_DATA=""
+export AZFB_TOKEN=""
+
+# CO
+export COFB_NAME=""
+export COFB_MGT=""
+export COFB_DATA=""
+export COFB_TOKEN=""
+
+# VA
+export VAFB_NAME=""
+export VAFB_MGT=""
+export VAFB_DATA=""
+export VAFB_TOKEN=""
