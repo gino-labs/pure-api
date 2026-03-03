27dc7ab69a2054f8095885ea2baccfc1917a27bb
CurtisGino <curtis.gino@rincon.com>
2026-03-03 15:21:24 -0700

    refactor: swapfbs script

--- src/swapfbs.py
+++ src/swapfbs.py
@@ -2,195 +2,218 @@
 from purefb_api import *
 from purefb_log import *
 import subprocess
-import json
+import yaml
 import os
 
-# Logger object 
-logger = PureLog()
-
-# Initialize Stopwatch object then start 
-timer = Stopwatch()
-timer.start_stopwatch()
-
-# Environment variables sourced from shell (site specific)
+# Initialize site variables and FB instances
 rrc_site = SiteVars()
 pb1_vars = rrc_site.get_pb1_vars()
 pb2_vars = rrc_site.get_pb2_vars()
 
-# FlashBlade API Object Instances 
 legacy = FlashBladeAPI(*pb1_vars)
 s200 = FlashBladeAPI(*pb2_vars)
 
-# Get Legacy file systems 
-legacy_filesystems = legacy.get_filesystems()
+# Get Filesystem info
+legacy_filesystems = legacy.get_filesystems()["items"]
+s200_filesystems = s200.get_filesystems()["items"]
 
-# Get S200 file systems 
-s200_filesystems = s200.get_filesystems()
+# Replication info
+replication_links = legacy.get_filesystem_replica_links()["items"]
+replication_filesystems = [ link["local_file_system"]["name"] for link in replication_links ]
 
-# Create promotion payloads using corresponding legacy file system
-s200_promo_payloads = {}
-for fs in legacy_filesystems:
-    s200_promo_payloads[fs["name"]] = {
-        "nfs": {
-            "v3_enabled": fs["nfs"]["v3_enabled"],
-            "v4_1_enabled": fs["nfs"]["v4_1_enabled"],
-            "rules": fs["nfs"]["rules"]
-        },
-        "writable": True,
-        "requested_promotion_state": "promoted" 
-    }
+# Get interface info
+legacy_interfaces = legacy.get_interfaces()["items"]
+s200_interfaces = s200.get_interfaces()["items"]
 
-# Get Legacy interfaces' info 
-legacy_interfaces = legacy.get_interfaces()
-
-legacy_data_iface_names = []
-legacy_data_ips = []
-
-for iface in legacy_interfaces:
-    if "data" in iface["services"] and "replication" not in iface["services"]:
-        legacy_data_iface_names.append(iface["name"])
-        legacy_data_ips.append(iface["address"])
-
-# Store Production IPs in variable to pass to ansible playbook later 
-if len(legacy_data_ips) > 1:
-    production_ips = "|".join(legacy_data_ips)
-elif len(legacy_data_ips) == 1:
-    production_ips = legacy_data_ips[0]
-else:
-    print("No data interfaces.")
-    print()
-
-# Get S200 interfaces' info
-s200_interfaces = s200.get_interfaces()
-
-s200_data_iface_names = [iface["name"] for iface in s200_interfaces if "data" in iface["services"]]
-
-# Match interfaces by same subnet 
-interfaces_matching_subnets = {}
-for iface200 in s200_interfaces:
-    if "data" in iface200["services"]:
-        iface200_subnet = iface200["subnet"]["name"]
-        for iface in legacy_interfaces:
-            if "data" in iface["services"] and iface200_subnet == iface["subnet"]["name"]:
-                interfaces_matching_subnets[iface["name"]] = iface200["name"]
-
-# Get file system replica links on Legacy
-replication_filesystems = [link["local_file_system"]["name"] for link in legacy.get_filesystem_replica_links()]
-
-# Get bucket replica links on legacy
-replication_buckets = [link["local_bucket"]["name"] for link in legacy.get_bucket_replia_links()]
-
-# Get active NFS clients before swapping ~30 second wait
-hosts = legacy.get_nfs_clients()
-
-# Create inventory file with NFS clients obtained
-inventory = {
-    "all": {
-        "hosts": {host["name"].split(":")[0]: None for host in hosts["items"] if "172.20." not in host["name"]} # FIXME?
-    }
-}
-
-inventory_filename = s200.mgt_ip[:2] + "_inventory.json"
-
-# TODO Evaluate inventory handling
+# Filepath variables
+inventory_file = "ansible/inventory/nfs_clients.yml"
+vars_file = "ansible/vars/swap_vars.yml"
 os.makedirs("ansible/inventory", exist_ok=True)
-with open(f"ansible/inventory/{inventory_filename}", "w") as inv_file:
-    json.dump(inventory, inv_file, indent=4)
+os.makedirs("ansible/vars", exist_ok=True)
 
-# Create final snapshots on Legacy and wait 30 seconds for them to settle #
-for fs in legacy_filesystems:
-    # TODO Evaluate error/handling
+# Take snapshots prior to swap
+def take_snapshots():
+    for fs in legacy_filesystems:
+        try:
+            if fs["promotion_status"] == "promoted":
+                legacy.post_filesystem_snapshot(fs["name"], "pre-swap")
+        except ApiError as e:
+            try:
+                legacy.post_filesystem_snapshot(fs["name"], "pre-swap", replicate=False) # Non-replication snapshot
+            except ApiError as e:
+                pass
+    time.sleep(20) # Wait time for snapshots to settle
+
+# Capture connected NFS clients into a inventory file
+def create_client_inventory():
     try:
-        if fs["promotion_status"] == "promoted":
-            legacy.post_filesystem_snapshot(fs["name"], "pre-swap")
+        nfs_clients = legacy.get_nfs_clients()["items"]
     except ApiError as e:
-        if e.code == 6:
-            legacy.post_filesystem_snapshot(fs["name"], "pre-swap", replicate=False) # Non-replication snapshot for applicable filesystems
-        elif e.code == 19:
-            pass # pre-swap snapshot exists
-        else:
-            e.check_details(show_code=True) # Error creating pre-swap snapshot
+        e.check_details()
 
-# TODO Notice of 30 second wait for snapshots
-timer.countdown(30)
-
-# Demote / Disable each file system on Legacy (Handle exception: non-replication snapshot error, skip demotion)
-for fs in legacy_filesystems:
-    # TODO Evaluate error/handling
-    try:
-        demote_payload = {
-            "writable": False,
-            "requested_promotion_state": "demoted"
+    inventory = {
+        "all": {
+            "hosts": {}
         }
-        legacy.patch_filesystem(fs["name"], demote_payload)
-    except ApiError as e:
-        if e.code == 32:
-            demote_payload = {
-                "writable": False
+    }
+    hosts = inventory["all"]["hosts"]
+
+    for client in nfs_clients:
+        client_addr = client["name"].split(":")[0]
+        if "172.20." not in client_addr:
+            hosts[client_addr] = None
+
+    # Validate inventory
+    if hosts == {}:
+        raise ValueError("Empty Inventory.")
+    elif any("172.20." in host for host in hosts.keys()):
+        raise ValueError(f"Replication Address in Inventory.")
+    else:
+        with open(inventory_file, "w") as f:
+            yaml.safe_dump(inventory, f, indent=2)
+
+# Capture production IP's into a vars file
+def save_swap_vars():
+    swap_vars = {
+        "production": [],
+        "secondary": []
+    }
+    for interface in legacy_interfaces:
+        if "data" in interface["services"]:
+            dict_item = {
+                "interface_name": interface["name"],
+                "address": interface["address"],
+                "subnet_name": interface["subnet"]["name"],
+                "vlan": interface["vlan"] 
             }
-            legacy.patch_filesystem(fs["name"], demote_payload) # Set unwritable only
-        else:
+            swap_vars["production"].append(dict_item)
+
+    for interface in s200_interfaces:
+        if "data" in interface["services"]:
+            dict_item = {
+                "interface_name": interface["name"],
+                "address": interface["address"],
+                "subnet_name": interface["subnet"]["name"],
+                "vlan": interface["vlan"] 
+            }
+            swap_vars["secondary"].append(dict_item)
+
+    # Validate production interfaces present
+    if swap_vars["production"] == []:
+        raise ValueError("Production Data Interface List is Empty.")
+    else:
+        with open(vars_file, "w") as f:
+            yaml.safe_dump(swap_vars, f, indent=2)
+
+# Demote Legacy Filesystems
+def demote_legacy_filesystems():
+    for fs in legacy_filesystems:
+        try:
+            demote_payload = {
+                "writable": False,
+                "requested_promotion_state": "demoted"
+            }
+            legacy.patch_filesystem(fs["name"], demote_payload)
+        except ApiError:
+            try:
+                no_write_payload = { "writable": False }
+                legacy.patch_filesystem(fs["name"], no_write_payload)
+            except ApiError as e:
+                e.check_details()
+
+# Swap Production IPs to S200
+def swap_production_ips_to_s200():
+    # Find a matching subnet
+    with open(vars_file, "r") as f:
+        swap_vars = yaml.safe_load(f)
+
+    for product in swap_vars["production"]:
+        production_ip = { "address": product["address"] }
+        # Check if production interface has a matching interface on secondary
+        match = False
+        for second in swap_vars["secondary"]:
+            if product["name"] == second["name"] or product["subnet"] == second["subnet"]:
+                secondary_ip = { "address": second["address"] }
+                try:
+                    s200.patch_interface(second["name"], production_ip)
+                    legacy.patch_interface(product["name"], secondary_ip)
+                except ApiError as e:
+                    e.check_details()
+                match = True
+                break
+        # Create production interface on s200
+        if not match:
+            production_info = {
+                "address": product["address"],
+                "services": [
+                    "data"
+                ],
+                "type": "vip"
+            }
+            try:
+                # Post to S200 and Delete from Legacy
+                s200.post_interface(product["name"], production_info)
+                legacy.delete_interface(product["name"])
+            except ApiError as e:
+                e.check_details()
+
+# Promote S200 Filesystems
+def promote_s200_filesystems():
+    # Assumming 1 to 1 match of filesystem names
+    for fs in legacy_filesystems:
+        try:
+            promote_payload = {
+                "nfs": {
+                    "v3_enabled": fs["nfs"]["v3_enabled"],
+                    "v4_1_enabled": fs["nfs"]["v4_1_enabled"],
+                    "rules": fs["nfs"]["rules"]
+                },
+                "writable": True,
+                "requested_promotion_state": "promoted" 
+            }
+            s200.patch_filesystem(fs["name"], promote_payload)
+        except ApiError as e:
             e.check_details()
 
-# Patch/Post Legacy IPs to S200
-post_ifaces = []
-for iface in legacy_interfaces:
-    if iface["name"] in s200_data_iface_names:
-        payload = { "address": iface["address"] }
-        s200.patch_interface(iface["name"], payload)
-    # Use matching subnet interface info
-    elif iface["name"] in interfaces_matching_subnets:
-        # Patch s200 interface instead with matching subnet
-        payload = { "address": iface["address"] }
-        s200.patch_interface(interfaces_matching_subnets[iface["name"]], payload)
-    else:
-        # Post/create interface if not exists # New for AZ
-        if "data" in iface["services"] and "replication" not in iface["services"]:
-            # New iface name <subnet-name>-interface
-            if "-subnet" in iface["subnet"]["name"]:
-                new_iface_name = iface["subnet"]["name"].replace("-subnet", "-interface")
-            else:
-                new_iface_name = iface["subnet"]["name"] + "-interface"
-            post_ifaces.append(iface)
-            payload = { "address": iface["address"], "services": ["data"], "type": "vip" }
-            s200.post_interface(new_iface_name, payload) 
+# Delete Replication links
+def delete_replication_links():
+    for link in replication_links:
+        fs = link["local_file_system"]["name"]
+        remote = link["remote"]["name"]
+        try:
+            legacy.delete_filesystem_replica_link(fs, remote)
+        except ApiError as e:
+            e.check_details()
 
-# Patch S200 IPs to Legacy
-for iface in s200_interfaces:
-    payload = { "address": iface["address"] }
-    if iface["name"] in legacy_data_iface_names:
-        legacy.patch_interface(iface["name"], payload)
-    elif iface["name"] in interfaces_matching_subnets.values():
-        for key, value in interfaces_matching_subnets.items():
-            if value == iface["name"]:
-                legacy.patch_interface(key, payload)
-        
-# Delete clean up legacy ips that were posted instead of patched
-for iface in post_ifaces:
-    legacy.delete_interface(iface)
+# Run Ansible playbook with inventory of clients and variables captured
+def remount_nfs_clients():
+    playbook = ["ansible-playbook", "-i", inventory_file, "-k", "remount-pure.yml"]
+    print("Enter root password for ansible playbook.")
+    subprocess.run(playbook, cwd="ansible")
 
-# Delete file system replication links on Legacy
-legacy_array_connections = legacy.get_array_connections()
 
-remote_array = legacy_array_connections["remote"]["name"]
+# MAIN
+if __name__ == "__main__":
+    # 1.) Take pre-swap snapshots (20 Second Settle)
+    take_snapshots()
 
-for filesystem in replication_filesystems:
-    legacy.delete_filesystem_replica_link(filesystem, remote_array)
+    # 2.) Generate inventory file from connected NFS clients
+    create_client_inventory()
 
-# Delete file system replication links on Legacy
-for bucket in replication_buckets:
-    legacy.delete_bucket_replica_link(bucket, remote_array)
+    # 3.) Store swap variables e.g., IPs
+    save_swap_vars()
 
-# Promote / Enable each file system on S200
-for fs in s200_filesystems:
-    if fs["name"] in s200_promo_payloads and not fs["destroyed"]:       
-        s200.patch_filesystem(fs["name"], s200_promo_payloads[fs["name"]])
+    # 4.) Demote Legacy FlashBlade file systems
+    demote_legacy_filesystems()
 
-# Run ansible playbook with nfs client inventory and production IP variable
-print("Enter root password for ansible playbook.")
+    # 5.) SWAP PRODUCTION IPs TO S200
+    swap_production_ips_to_s200()
 
-# FIXME Hard-Coded inventory
-subprocess.run(["ansible-playbook", "-i", f"inventory/az_inventory.json", "-e", f"pure_ips={production_ips}", "-k", "remount-pure.yml"], cwd="ansible")
+    # 6.) Promote S200 FlashBlade file systems
+    promote_s200_filesystems()
 
-# End stopwatch for script run time
-timer.end_stopwatch()
+    # 7.) Delete replication links from Legacy
+    delete_replication_links()
+
+    # 8.) Ansible playbook to remount NFS shares from NFS client inventory
+    remount_nfs_clients()
