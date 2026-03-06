67fb247f267d73636906f025e12016c4d935c2a5
CurtisGino <curtis.gino@rincon.com>
2026-03-06 12:05:22 -0700

    refactor: prefix with underscore on request methods

--- src/flashblade/api/_get.py
+++ src/flashblade/api/_get.py
@@ -5,7 +5,7 @@ class FBGet:
         self.session = session
         self.session.verify = verify
 
-    def get_request(self, endpoint, params=None, only_items=True):
+    def _get_request(self, endpoint, params=None, only_items=True):
         url = f"{self.session.baseurl}/{endpoint}"
         response = self.session.get(url, params=params)
         response.raise_for_status()
@@ -15,31 +15,31 @@ class FBGet:
             return response.json()
         
     def get_alert_watchers(self, **params):
-        return self.get_request("alert-watchers", params=params)
+        return self._get_request("alert-watchers", params=params)
 
     def get_array_connections(self, sub_resource=None, **params):
         ep = "array-conections" if not sub_resource else f"array-connections/{sub_resource}"
-        return self.get_request(ep, params=params)
+        return self._get_request(ep, params=params)
     
     def get_arrays(self, sub_resource=None, **params):
         ep = "arrays" if not sub_resource else f"arrays/{sub_resource}"
-        return self.get_request(ep, params=params)
+        return self._get_request(ep, params=params)
 
     def get_bucket_replica_links(self, **params):
-        return self.get_request("bucket-replica-links", params=params)
+        return self._get_request("bucket-replica-links", params=params)
                 
     def get_buckets(self, **params):
-        return self.get_request("buckets", params=params)
+        return self._get_request("buckets", params=params)
         
     def get_certificate_group_members(self, **params):
         return self.get_certificate_groups(sub_resource="certificates", params=params)
         
     def get_certificate_groups(self, sub_resource=None, **params):
         ep = "certificate-groups" if not sub_resource else f"certificate-groups/{sub_resource}"
-        return self.get_request(ep, params=params)
+        return self._get_request(ep, params=params)
     
     def get_certificates(self, **params):
-        return self.get_request("certificates", params=params)
+        return self._get_request("certificates", params=params)
        
     def get_clients(self, **params):
         return self.get_arrays(sub_resource="clients/performance", params=params)
@@ -49,75 +49,75 @@ class FBGet:
         
     def get_directory_services(self, sub_resource=None, **params):
         ep = "directory-services" if not sub_resource else f"directory-services/{sub_resource}"
-        return self.get_request(ep, params=params)
+        return self._get_request(ep, params=params)
         
     def get_dns(self, **params):
-        return self.get_request("dns", params=params)
+        return self._get_request("dns", params=params)
        
     def get_filesystem_replica_links(self, **params):
-        return self.get_request("file-system-replica-links", params=params)
+        return self._get_request("file-system-replica-links", params=params)
         
     def get_filesystem_snapshots(self, **params):
-        return self.get_request("file-system-snapshots", params=params)
+        return self._get_request("file-system-snapshots", params=params)
             
     def get_filesystems(self, sub_resource=None, **params):
         ep = "file-systems" if not sub_resource else f"file-systems/{sub_resource}"
-        return self.get_request(ep, params=params)
+        return self._get_request(ep, params=params)
 
     def get_object_store_access_keys(self, **params):
-        return self.get_request("object-store-access-keys", params=params)
+        return self._get_request("object-store-access-keys", params=params)
              
     def get_object_store_accounts(self, **params):
-        return self.get_request("object-store-accounts", params=params)
+        return self._get_request("object-store-accounts", params=params)
 
     def get_object_store_remote_credentials(self, **params):
-        return self.get_request("object-store-remote-credentials", params=params)
+        return self._get_request("object-store-remote-credentials", params=params)
     
     def get_object_store_users(self, **params):
-        return self.get_request("object-store-users", params)
+        return self._get_request("object-store-users", params)
         
     def get_network_interfaces(self, params):
-        return self.get_request("network-interfaces", params=params)
+        return self._get_request("network-interfaces", params=params)
     
     def get_nfs_export_policies(self, **params):
-        return self.get_request("nfs-export-policies", params=params)
+        return self._get_request("nfs-export-policies", params=params)
     
     def get_quotas_groups(self, **params):
-        return self.get_request("quotas/groups", params=params)    
+        return self._get_request("quotas/groups", params=params)    
     
     def get_quotas_settings(self, **params):
-        return self.get_request("quotas/settings", params=params)
+        return self._get_request("quotas/settings", params=params)
         
     def get_quotas_users(self, **params):
-        return self.get_request("quotas/users", params=params)
+        return self._get_request("quotas/users", params=params)
        
     def get_roles(self, **params):
         return self.get_directory_services(sub_resource="roles", params=params)
         
     def get_smb_client_policies(self, **params):
-        return self.get_request("smb-client-policies", params=params)
+        return self._get_request("smb-client-policies", params=params)
       
     def get_smb_share_policies(self, **params):
-        return self.get_request("smb-share-policies", params=params)
+        return self._get_request("smb-share-policies", params=params)
         
     def get_smtp_servers(self, **params):
-        return self.get_request("smtp-servers", params=params)
+        return self._get_request("smtp-servers", params=params)
         
     def get_snapshot_policies(self, **params):
-        return self.get_request("policies", params=params)
+        return self._get_request("policies", params=params)
     
     def get_snapshot_policy_members(self, **params):
         return self.get_filesystems(sub_resource="policies", params=params)
            
     def get_subnets(self, **params):
-        return self.get_request("subnets", params=params)
+        return self._get_request("subnets", params=params)
 
     def get_syslog_servers(self, **params):
-        return self.get_request("syslog-servers", params=params)
+        return self._get_request("syslog-servers", params=params)
 
     def get_usage_groups(self, **params):
-        return self.get_request("usage/groups", params=params)
+        return self._get_request("usage/groups", params=params)
            
     def get_usage_users(self, **params):
-        return self.get_request("usage/users", params=params)
+        return self._get_request("usage/users", params=params)
     
