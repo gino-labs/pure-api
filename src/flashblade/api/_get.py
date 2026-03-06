3ecf8474f15a15618e369c2587a811becb004dfe
CurtisGino <curtis.gino@rincon.com>
2026-03-06 10:02:04 -0700

    feat: FBGet class consumable

--- src/flashblade/api/_get.py
+++ src/flashblade/api/_get.py
@@ -13,347 +13,111 @@ class FBGet:
             return response.json()["items"]
         else:
             return response.json()
-    
-    def get_filesystems(self, **params):
-        return self._request("file-systems", params=params)
         
+    def get_alert_watchers(self, **params):
+        return self._request("alert-watchers", params=params)
+
+    def get_array_connections(self, sub_resource=None, **params):
+        ep = "array-conections" if not sub_resource else f"array-connections/{sub_resource}"
+        return self._request(ep, params=params)
+    
+    def get_arrays(self, sub_resource=None, **params):
+        ep = "arrays" if not sub_resource else f"arrays/{sub_resource}"
+        return self._request(ep, params=params)
+
+    def get_bucket_replica_links(self, **params):
+        return self._request("bucket-replica-links", params=params)
+                
+    def get_buckets(self, **params):
+        return self._request("buckets", params=params)
+        
+    def get_certificate_group_members(self, **params):
+        return self.get_certificate_groups(sub_resource="certificates", params=params)
+        
+    def get_certificate_groups(self, sub_resource=None, **params):
+        ep = "certificate-groups" if not sub_resource else f"certificate-groups/{sub_resource}"
+        return self._request(ep, params=params)
+    
+    def get_certificates(self, **params):
+        return self._request("certificates", params=params)
+       
+    def get_clients(self, **params):
+        return self.get_arrays(sub_resource="clients/performance", params=params)
+        
+    def get_connection_key(self, **params):
+        return self.get_array_connections(sub_resource="connection-key", params=params)
+        
+    def get_directory_services(self, sub_resource=None, **params):
+        ep = "directory-services" if not sub_resource else f"directory-services/{sub_resource}"
+        return self._request(ep, params=params)
+        
+    def get_dns(self, **params):
+        return self._request("dns", params=params)
+       
+    def get_filesystem_replica_links(self, **params):
+        return self._request("file-system-replica-links", params=params)
+        
+    def get_filesystem_snapshots(self, **params):
+        return self._request("file-system-snapshots", params=params)
+            
+    def get_filesystems(self, sub_resource=None, **params):
+        ep = "file-systems" if not sub_resource else f"file-systems/{sub_resource}"
+        return self._request(ep, params=params)
+
+    def get_object_store_access_keys(self, **params):
+        return self._request("object-store-access-keys", params=params)
+             
     def get_object_store_accounts(self, **params):
         return self._request("object-store-accounts", params=params)
-        
-    # Get buckets
-    def get_buckets(self, buckets=None, dumpjson=False):
-        if buckets is not None:
-            buck_list = self.to_csv(buckets)
-            url = self.baseurl + f"buckets?names={buck_list}"
-            msg = f"buckets: {buck_list}"
-        else:
-            url = self.baseurl + "buckets"
-            msg = "buckets"
 
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
+    def get_object_store_remote_credentials(self, **params):
+        return self._request("object-store-remote-credentials", params=params)
+    
+    def get_object_store_users(self, **params):
+        return self._request("object-store-users", params)
         
-    # Get bucket replica links
-    def get_bucket_replia_links(self, buckets=None, dumpjson=False):
-        if buckets is not None:
-            buck_list = self.to_csv(buckets)
-            url = self.baseurl + f"bucket-replica-links?local_bucket_names={buck_list}"
-            msg = f"bucket replica links: {buck_list}"
-        else:
-            url = self.baseurl + f"bucket-replica-links"
-            msg = f"bucket replica links"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
+    def get_network_interfaces(self, params):
+        return self._request("network-interfaces", params=params)
+    
+    def get_nfs_export_policies(self, **params):
+        return self._request("nfs-export-policies", params=params)
+    
+    def get_quotas_groups(self, **params):
+        return self._request("quotas/groups", params=params)    
+    
+    def get_quotas_settings(self, **params):
+        return self._request("quotas/settings", params=params)
         
-    # Get object store users
-    def get_object_store_users(self, users=None, dumpjson=False):
-        if users is not None:
-            user_list = self.to_csv(users)
-            url = self.baseurl + f"object-store-users?names={user_list}"
-            msg = f"object store users: {users}"
-        else:
-            url = self.baseurl + f"object-store-users"
-            msg = f"object store users"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-
-    # Get object store access keys
-    def get_object_store_access_keys(self, keys=None, dumpjson=False):
-        if keys is not None:
-            key_list = self.to_csv(keys)
-            url = self.baseurl + f"object-store-access-keys?names={key_list}"
-            msg = f"object store access keys: {key_list}"
-        else:
-            url = self.baseurl + f"object-store-access-keys"
-            msg = "object store access keys"
-        
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-        
-    # Get object store remote credentials
-    def get_object_store_remote_credentials(self, credentials=None, dumpjson=False):
-        if credentials is not None:
-            cred_list = self.to_csv(credentials)
-            url = self.baseurl + f"object-store-remote-credentials?names={cred_list}"
-            msg = f"object store remote credentials: {cred_list}"
-        else:
-            url = self.baseurl + f"object-store-remote-credentials"
-            msg = f"object store remote credentials"
-            
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-
-    # Get subnets
-    def get_subnets(self, subnets=None, dumpjson=False):
-        if subnets is not None:
-            sub_list = self.to_csv(subnets)
-            url = self.baseurl + f"subnets?names={sub_list}"
-            msg = f"subnets: {sub_list}"
-        else:
-            url = self.baseurl + "subnets"
-            msg = "subnets"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-        
-    # Get network interfaces
-    def get_interfaces(self, interfaces=None, dumpjson=False):
-        if interfaces is not None:
-            iface_list = self.to_csv(interfaces)
-            url = self.baseurl + f"network-interfaces?names={iface_list}"
-            msg = f"network interfaces: {iface_list}"
-        else:
-            url = self.baseurl + "network-interfaces"
-            msg = "network interfaces"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-
-    # Get filesystem replica links
-    def get_filesystem_replica_links(self, filesystems=None, dumpjson=False):
-        if filesystems is not None:
-            fs_list = self.to_csv(filesystems)
-            url = self.baseurl + f"file-system-replica-links?local_file_system_names={fs_list}" 
-            msg = f"filesystem replica links: {fs_list}"
-        else:
-            url = self.baseurl + "file-system-replica-links" 
-            msg = "filesystem replica links"
-        
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-        
-    # Get filesystem snapshots
-    def get_filesystem_snapshots(self, snapshots=None, dumpjson=False):
-        if snapshots is not None:
-            snap_list = self.to_csv(snapshots)
-            url = self.baseurl + f"file-system-snapshots?names_or_owner_names={snap_list}"
-            msg = f"filesystem snapshots: {snap_list}"
-        else:
-            url = self.baseurl + f"file-system-snapshots"
-            msg = f"filesystem snapshots"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-        
-    # Get snapshot polices
-    def get_snapshot_policies(self, policies=None, dumpjson=False):
-        if policies is not None:
-            pol_list = self.to_csv(policies)
-            url = self.baseurl + f"policies?names={pol_list}"
-            msg = f"filesystem snapshot policies: {pol_list}"
-        else:
-            url = self.baseurl + "policies"
-            msg = "filesystem snapshot policies"
-        
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-        
-    # Get snapshot policies attached 
-    def get_snapshot_policy_members(self, policies=None, dumpjson=False):
-        if policies is not None:
-            pol_list = self.to_csv(policies)
-            url = self.baseurl + f"file-systems/policies?policy_names={pol_list}"
-            msg = f"attached snapshot policy: {pol_list}"
-        else:
-            url = self.baseurl + f"file-systems/policies"
-            msg = f"attached snapshot policies"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
+    def get_quotas_users(self, **params):
+        return self._request("quotas/users", params=params)
        
-    # Get connected NFS clients
-    def get_nfs_clients(self, dumpjson=False):
-        url = self.baseurl + "arrays/clients/performance"
-        msg = "NFS clients"
-        data = self.api_request("get", url, msg)
-        return data
+    def get_roles(self, **params):
+        return self.get_directory_services(sub_resource="roles", params=params)
         
-    # Get remote array connections
-    def get_array_connections(self, dumpjson=False):
-        url = self.baseurl + "array-connections"
-        msg = "array connections"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get directory services
-    def get_directory_services(self, dumpjson=False):
-        url = self.baseurl + "directory-services"
-        msg = "directory services"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get directory service roles
-    def get_directory_service_roles(self, role_names=None, dumpjson=False):
-        if role_names is not None:
-            role_list = self.to_csv(role_names)
-            url = self.baseurl + f"directory-services/roles?role_names={role_list}"
-            msg = f"directory service roles: {role_names}"
-        else:
-            url = self.baseurl + "directory-services/roles"
-            msg = "directory service roles"
+    def get_smb_client_policies(self, **params):
+        return self._request("smb-client-policies", params=params)
+      
+    def get_smb_share_policies(self, **params):
+        return self._request("smb-share-policies", params=params)
         
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get DNS configuration
-    def get_dns(self, dumpjson=False):
-        url = self.baseurl + "dns"
-        msg = "DNS"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get array configurations
-    def get_array_configurations(self, dumpjson=False):
-        url = self.baseurl + "arrays"
-        msg = "array configurations"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get array connections
-    def get_array_connections(self, dumpjson=False):
-        url = self.baseurl + "array-connections"
-        msg = "array connections"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get certifcates
-    def get_certificates(self, certificates=None, dumpjson=False):
-        if certificates is not None:
-            cert_list = self.to_csv(certificates)
-            url = self.baseurl + f"certificates?names={cert_list}"
-            msg = f"certificates: {certificates}"
-        else:
-            url = self.baseurl + "certificates"
-            msg = "certifcates"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get certificate groups
-    def get_certificate_groups(self, groups=None, dumpjson=False):
-        if groups is not None:
-            group_list = self.to_csv(groups)
-            url = self.baseurl + f"certificate-groups?names={group_list}"
-            msg = f"certificate groups: {groups}"
-        else:
-            url = self.baseurl + "certificate-groups"
-            msg = "certificate groups"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get certificate group memebers
-    def get_certificate_group_members(self, group, dumpjson=False):
-        url = self.baseurl + f"certificate-groups/certificates?certificate_group_names={group}"
-        msg = "certificate group members"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get NFS export policies
-    def get_nfs_export_policies(self, policies=None, dumpjson=False):
-        if policies is not None:
-            pol_list = self.to_csv(policies)
-            url = self.baseurl + f"nfs-export-policies?names={pol_list}"
-            msg = f"nfs export policies: {pol_list}"
-        else:
-            url = self.baseurl + "nfs-export-policies"
-            msg = "nfs export policies"
+    def get_smtp_servers(self, **params):
+        return self._request("smtp-servers", params=params)
         
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
+    def get_snapshot_policies(self, **params):
+        return self._request("policies", params=params)
     
-    # Get SMB client policies
-    def get_smb_client_policies(self, policies=None, dumpjson=False):
-        if policies is not None:
-            pol_list = self.to_csv(policies)
-            url = self.baseurl + f"smb-client-policies?names={pol_list}"
-            msg = f"smb client policies: {pol_list}"
-        else:
-            url = self.baseurl + "smb-client-policies"
-            msg = "smb client policies"
-        
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get SMB share policies
-    def get_smb_share_policies(self, policies=None, dumpjson=False):
-        if policies is not None:
-            pol_list = self.to_csv(policies)
-            url = self.baseurl + f"smb-share-policies?names={pol_list}"
-            msg = f"smb share policies: {pol_list}"
-        else:
-            url = self.baseurl + "smb-share-policies"
-            msg = "smb share policies"
-        
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get syslog servers
-    def get_syslog_servers(self, syslog_names=None, dumpjson=False):
-        if syslog_names is not None:
-            syslog_list = self.to_csv(syslog_names)
-            url = self.baseurl + f"syslog-servers?names={syslog_list}"
-            msg = f"syslog servers: {syslog_list}"
-        else:
-            url = self.baseurl + f"syslog-servers"
-            msg = f"syslog servers"
+    def get_snapshot_policy_members(self, **params):
+        return self.get_filesystems(sub_resource="policies", params=params)
+           
+    def get_subnets(self, **params):
+        return self._request("subnets", params=params)
 
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get connection key
-    def get_connection_key(self, dumpjson=False):
-        url = self.baseurl + "array-connections/connection-key"
-        msg = "connection key"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get users quotas per file system
-    def get_user_quotas(self, filesystem, dumpjson=False):
-        url = self.baseurl + f"quotas/users?file_system_names={filesystem}"
-        msg = f"{filesystem} user quotas"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get users file system usage
-    def get_users_filesystem_usage(self, filesystem, uids=None, dumpjson=False):
-        if uids is not None:
-            uid_list = self.to_csv(uids)
-            url = self.baseurl + f"usage/users?file_system_names={filesystem}&uids={uid_list}"
-            msg = f"{filesystem} usage for UIDs: {uid_list}"
-        else:
-            url = self.baseurl + f"usage/users?file_system_names={filesystem}"
-            msg = f"{filesystem} usage by UIDs"
+    def get_syslog_servers(self, **params):
+        return self._request("syslog-servers", params=params)
 
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
+    def get_usage_groups(self, **params):
+        return self._request("usage/groups", params=params)
+           
+    def get_usage_users(self, **params):
+        return self._request("usage/users", params=params)
     
-    # Get alert watchers
-    def get_alert_watchers(self, watchers=None, dumpjson=False):
-        if watchers is not None:
-            watcher_list = self.to_csv(watchers)
-            url = self.baseurl + f"alert-watchers?names={watcher_list}"
-            msg = f"alert watchers: {watcher_list}"
-        else:
-            url = self.baseurl + "alert-watchers"
-            msg = f"alert watchers"
-
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get smtp servers
-    def get_smtp_servers(self, dumpjson=False):
-        url = self.baseurl + "smtp-servers"
-        msg = f"smtp servers"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-    
-    # Get quotas settings
-    def get_quotas_settings(self, dumpjson=False):
-        url = self.baseurl + "quotas/settings"
-        msg = f"quotas settings"
-        data = self.api_request("get", url, msg)
-        return self.parse_data(data, dumpjson=dumpjson)
-
