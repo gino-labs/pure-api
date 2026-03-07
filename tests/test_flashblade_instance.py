3f2f8dd1c9e8acad91a94ef4b496f9315fb8201b
CurtisGino <curtis.gino@rincon.com>
2026-03-07 14:52:13 -0700

    fix: test for lower case variable names

--- tests/test_flashblade_instance.py
+++ tests/test_flashblade_instance.py
@@ -4,7 +4,7 @@ def test_env_reader_instance():
     ev_list = ["FB_NAME", "MGT_IP", "API_TOKEN"]
     fbvars = EnvironmentReader(*ev_list)
     for v in ev_list:
-        assert v == fbvars.get_var(v)
+        assert v.lower() == fbvars.get_var(v)
 
 #def test_flashblade_api_instance():
 #    fb = FlashBladeAPI()
