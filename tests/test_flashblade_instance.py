7a00710606499a3b6c083ad7706ad62d645de58b
CurtisGino <curtis.gino@rincon.com>
2026-03-07 14:39:10 -0700

    feat: write test for environment variables

--- tests/test_flashblade_instance.py
+++ tests/test_flashblade_instance.py
@@ -1,5 +1,11 @@
+from everpure import *
 
+def test_env_reader_instance():
+    ev_list = ["FB_NAME", "MGT_IP", "API_TOKEN"]
+    fbvars = EnvironmentReader(*ev_list)
+    for v in ev_list:
+        assert v == fbvars.get_var(v)
 
-def test_flashblade_api_instance():
-    pass
+#def test_flashblade_api_instance():
+#    fb = FlashBladeAPI()
 
