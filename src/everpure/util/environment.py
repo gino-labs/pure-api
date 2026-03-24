84c7785ea22e84fb626515b3849f0785cb846a56
CurtisGino <curtis.gino@rincon.com>
2026-03-24 09:17:05 -0700

    refactor: EnvironmentReader class

--- src/everpure/util/environment.py
+++ src/everpure/util/environment.py
@@ -1,8 +1,11 @@
 import os
 
 class EnvironmentReader:
-    def __init__(self, name: str, mgt: str, token: str):
-        self.name = name
-        self.mgt = mgt
-        self.token = token
+    def __init__(self, name: str, mgt: str, token: str, *args):
+        self.name = os.environ[name]
+        self.mgt = os.environ[mgt]
+        self.token = os.environ[token]
+        if args:
+            for arg in args:
+                setattr(self, arg.lower(), arg)
 
