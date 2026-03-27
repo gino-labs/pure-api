ea5df19a8f9b9180b04d9539df4ffe3788ce0461
CurtisGino <curtis.gino@rincon.com>
2026-03-27 14:26:42 -0700

    update: test environment

--- tests/test_fb_api_environment.py
+++ tests/test_fb_api_environment.py
@@ -3,20 +3,17 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def test_env_reader_instance():
-    ev_list = ["AZFB_NAME", "AZFB_MGT", "AZFB_TOKEN"]
-    fbenv = EnvironmentReader(*ev_list)
-    for v in ev_list:
-        assert v.lower() == fbenv.get_var(v)
-
 def test_flashblade_api_instance():
     fbenv = EnvironmentReader("AZFB_NAME", "AZFB_MGT", "AZFB_TOKEN")
-    assert fbenv.azfb_name
-    assert fbenv.azfb_mgt
-    assert fbenv.azfb_token
-    fbvars = [fbenv.azfb_name, fbenv.azfb_mgt, fbenv.azfb_token]
+    assert fbenv.name
+    assert fbenv.mgt
+    assert fbenv.token
+    fbvars = [fbenv.name, fbenv.mgt, fbenv.token]
     fb = FlashBladeAPI(*fbvars)
-    assert fb.name == fbenv.azfb_name
-    assert fb.session.mgt == fbenv.azfb_mgt
-    assert fb.session.token == fbenv.azfb_token
+    assert fb.name == fbenv.name
+    assert fb.session.mgt == fbenv.mgt
+    assert fb.session.token == fbenv.token
 
+    arrays = fb.get_arrays()
+    assert isinstance(arrays, list)
+    assert len(arrays) > 0
