1c4b3a3cdcbef4fdc27628c28c4dd46391dfeabd
Gino Curtis <ginocurtis5@gmail.com>
2026-05-07 13:38:29 -0700

    refactor: args -> kwargs for dynamic attrbute naming

--- src/everpure/util/environment.py
+++ src/everpure/util/environment.py
@@ -1,14 +1,14 @@
 import os
 
 class EnvironmentReader:
-    def __init__(self, name: str, mgt: str, token: str, *args):
+    def __init__(self, name: str, mgt: str, token: str, **kwargs):
         self.name = os.environ[name]
         self.mgt = os.environ[mgt]
         self.token = os.environ[token]
         self.vars = [self.name, self.mgt, self.token]
-        if args:
-            for arg in args:
-                setattr(self, arg.lower(), arg)
+        if kwargs:
+            for k, w in kwargs.items():
+                setattr(self, k, w)
 
     def __iter__(self):
         return iter(self.vars)
